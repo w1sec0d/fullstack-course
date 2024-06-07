3d8057e5223248b1569d46bc6a671f44b00b0719
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-06 23:27:16 -0500

    add exercise 2.8: The Phonebook Step 3

--- part2-communicating-with-server/phonebook/src/App.jsx
+++ part2-communicating-with-server/phonebook/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", phone: "040-1234567" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newPhone, setNewPhone] = useState("");
 
   return (
     <div>
@@ -13,9 +16,10 @@ const App = () => {
           if (persons.some((obj) => obj.name == newName)) {
             window.alert(`${newName} is already added on the notebook!`);
           } else {
-            setPersons(persons.concat({ name: newName }));
+            setPersons(persons.concat({ name: newName, phone: newPhone }));
           }
           setNewName("");
+          setNewPhone("");
         }}
       >
         <div>
@@ -26,12 +30,21 @@ const App = () => {
           />
         </div>
         <div>
+          number:{" "}
+          <input
+            value={newPhone}
+            onChange={(event) => setNewPhone(event.target.value)}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
+        <p key={person.name}>
+          {person.name} {person.phone}
+        </p>
       ))}
     </div>
   );
