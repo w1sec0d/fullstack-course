18194d01063fbe64349e5bd8d00f1a8555daf176
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-06 23:08:58 -0500

    Add exercise 2.6: The Phonebook Step 1

---
+++ part2-communicating-with-server/phonebook/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setPersons(persons.concat({ name: newName }));
+        }}
+      >
+        <div>
+          name:{" "}
+          <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
