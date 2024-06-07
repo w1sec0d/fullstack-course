c983808013574fc052b3b2800e174eed582d4b0c
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-06 23:35:04 -0500

    Add exercise 2.9*: The Phonebook Step 4

--- part2-communicating-with-server/phonebook/src/App.jsx
+++ part2-communicating-with-server/phonebook/src/App.jsx
@@ -2,14 +2,30 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", phone: "040-1234567" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newPhone, setNewPhone] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const filteredPeople = persons.filter((person) =>
+    person.name.toLowerCase().startsWith(filter.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with
+        <input
+          value={filter}
+          onChange={(event) => setFilter(event.target.value)}
+        />
+      </div>
+      <h2>Add a New Contact</h2>
       <form
         onSubmit={(event) => {
           event.preventDefault();
@@ -41,7 +57,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filteredPeople.map((person) => (
         <p key={person.name}>
           {person.name} {person.phone}
         </p>
