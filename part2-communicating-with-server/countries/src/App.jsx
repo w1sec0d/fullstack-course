be08b1f7929d4db4dd651157f8b57c26c622361b
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-10 23:18:09 -0500

    Add exercise 2.20*: Data for countries, step 3

--- part2-communicating-with-server/countries/src/App.jsx
+++ part2-communicating-with-server/countries/src/App.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from "react";
 import Country from "./components/Country";
+import axios from "axios";
 
 const App = () => {
   const [countries, setCountries] = useState({ all: null, filtered: [] });
--- part2-communicating-with-server/countries/src/components/Country.jsx
+++ part2-communicating-with-server/countries/src/components/Country.jsx
@@ -1,6 +1,26 @@
-import React from "react";
+import { useEffect, useState, useCallback } from "react";
+import axios from "axios";
 
 const Country = ({ value }) => {
+  const OPEN_WEATHER_API_KEY = import.meta.env.VITE_WEATHER_KEY;
+  const capitalCoords = value.capitalInfo.latlng;
+  const [weather, setWeater] = useState(null);
+
+  const getWeather = useCallback(
+    (lat, lon) => {
+      return axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
+        )
+        .then((res) => setWeater(res.data));
+    },
+    [OPEN_WEATHER_API_KEY]
+  );
+
+  useEffect(() => {
+    getWeather(capitalCoords[0], capitalCoords[1]);
+  }, [getWeather, capitalCoords]);
+
   return (
     <div>
       <h1>{value.name.common ? value.name.common : "Country name"}</h1>
@@ -17,6 +37,18 @@ const Country = ({ value }) => {
         ))}
       </ul>
       <img src={value.flags.svg} style={{ maxWidth: "200px" }} />
+      {weather && (
+        <>
+          <h2>Weather In {value.capital}</h2>
+          <p>Temperature: {weather.main.temp} Celsius</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={`Icon of weather: ${weather.weather[0].description}`}
+            title={weather.weather[0].description}
+          />
+          <p>Wind:{weather.wind.speed} m/s</p>
+        </>
+      )}
     </div>
   );
 };
