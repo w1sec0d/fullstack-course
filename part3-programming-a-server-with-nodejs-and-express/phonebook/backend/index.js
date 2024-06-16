2d93c347fcefb2deb53bd93d58cf66c603719384
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-16 12:12:06 -0500

    Add exercise 3.5: Phonebook backend step 5

--- part3-programming-a-server-with-nodejs-and-express/phonebook/backend/index.js
+++ part3-programming-a-server-with-nodejs-and-express/phonebook/backend/index.js
@@ -27,6 +27,27 @@ let persons = [
   },
 ];
 
+// Post routes
+app.post("/api/persons", (request, response) => {
+  let person = request.body;
+  if (person.name && person.number) {
+    // Ensures a unique id
+    let newId = Math.random();
+    while (persons.find((person) => person.id === newId)) {
+      newId = Math.random();
+    }
+
+    // Adds the new person
+    const newPerson = { id: newId, name: person.name, number: person.number };
+    persons = persons.concat(newPerson);
+
+    response.status(201).json(newPerson);
+  } else {
+    response.status(400).end();
+  }
+});
+
+// Get routes
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
@@ -46,6 +67,8 @@ app.get("/info", (request, response) => {
     `Phonebook has info for ${persons.length} people <br> ${new Date()}`
   );
 });
+
+// Delete routes
 
 app.delete("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
