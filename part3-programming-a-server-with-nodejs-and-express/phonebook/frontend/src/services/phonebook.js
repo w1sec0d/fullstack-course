21786b741b741547c1d42d15d8d69cf6072da0b9
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-16 22:30:33 -0500

    Add exercise 3.10 Phonebook backend step 10

--- part3-programming-a-server-with-nodejs-and-express/phonebook/frontend/src/services/phonebook.js
+++ part3-programming-a-server-with-nodejs-and-express/phonebook/frontend/src/services/phonebook.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "http://localhost:3001/api/persons";
+const baseURL = "/api/persons";
 
 const create = (newContact) => {
   const request = axios.post(baseURL, newContact);
