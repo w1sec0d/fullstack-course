2a343eb2b3928ea5292ae8a608c3461b6cdaada3
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-16 19:24:13 -0500

    Resolve local conflict

--- part3-programming-a-server-with-nodejs-and-express/notes/frontend/src/main.jsx
+++ part3-programming-a-server-with-nodejs-and-express/notes/frontend/src/main.jsx
@@ -1,25 +1,25 @@
-import ReactDOM from 'react-dom/client'
-
-import App from './App'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
 const notes = [
   {
     id: 1,
-    content: 'HTML is easy',
-    important: true
+    content: "HTML is easy",
+    important: true,
   },
   {
     id: 2,
-    content: 'Browser can execute only JavaScript',
-    important: false
+    content: "Browser can execute only JavaScript",
+    important: false,
   },
   {
     id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    important: true
-  }
-]
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <App notes={notes} />
-)
+);
