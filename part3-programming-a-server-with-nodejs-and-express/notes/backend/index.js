90a58bb61e0c1f251db060ab402e47b77d40631f
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-28 14:06:39 -0500

    Add put and delete routes

--- part3-programming-a-server-with-nodejs-and-express/notes/backend/index.js
+++ part3-programming-a-server-with-nodejs-and-express/notes/backend/index.js
@@ -20,6 +20,19 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
+app.get("/api/notes/:id", (request, response) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+
+    .catch((error) => next(error));
+});
+
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
@@ -40,21 +53,46 @@ app.post("/api/notes", (request, response) => {
     .catch((error) => console.log(error));
 });
 
-app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then((result) => {
+      response.status(204).end();
     })
-
-    .catch((error) => {
-      console.log(error);
-      response.status(500).end();
-    });
+    .catch((error) => next(error));
 });
+
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
