e9803986d465dab16a2f3ff097ad9c2d3601d864
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-06-28 19:07:50 -0500

    Adds note validation

--- part3-programming-a-server-with-nodejs-and-express/notes/backend/index.js
+++ part3-programming-a-server-with-nodejs-and-express/notes/backend/index.js
@@ -33,7 +33,7 @@ app.get("/api/notes/:id", (request, response) => {
     .catch((error) => next(error));
 });
 
-app.post("/api/notes", (request, response) => {
+app.post("/api/notes", (request, response, next) => {
   const body = request.body;
 
   if (body.content === undefined) {
@@ -50,7 +50,7 @@ app.post("/api/notes", (request, response) => {
     .then((savedNote) => {
       response.json(savedNote);
     })
-    .catch((error) => console.log(error));
+    .catch((error) => next(error));
 });
 
 app.delete("/api/notes/:id", (request, response, next) => {
@@ -69,7 +69,11 @@ app.put("/api/notes/:id", (request, response, next) => {
     important: body.important,
   };
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+    runValidators: true,
+    context: "query",
+  })
     .then((updatedNote) => {
       response.json(updatedNote);
     })
@@ -88,6 +92,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
