19a74ae5e60c63634882958223f40f71941225aa
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-07-07 20:43:38 -0500

    Add exercise 4.1 Blog List, step 1

--- part4-testing-express-servers-user-administration/notes/backend/models/note.js
+++ part4-testing-express-servers-user-administration/notes/backend/models/note.js
@@ -1,22 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URL
-
-mongoose
-  .connect(url)
-
-  .then(() => { console.log('connected to MongoDB') })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
 const noteSchema = new mongoose.Schema({
   content: {
     type: String,
-    minLength: 5,
     required: true,
+    minlength: 5
   },
   important: Boolean,
 })
@@ -26,7 +14,7 @@ noteSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-  },
+  }
 })
 
 module.exports = mongoose.model('Note', noteSchema)
