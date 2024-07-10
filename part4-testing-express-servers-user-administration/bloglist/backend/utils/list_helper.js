ffb603629e99fd5da8116460a584a126ce4aed3b
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-07-10 11:38:30 -0500

    Add exercise 4.5*: Helper Functions and Unit Tests, step 3

--- part4-testing-express-servers-user-administration/bloglist/backend/utils/list_helper.js
+++ part4-testing-express-servers-user-administration/bloglist/backend/utils/list_helper.js
@@ -1,12 +1,27 @@
 const dummy = (blogs) => {
-  return 1
-}
+  return 1;
+};
 
-const totalLikes = (blogs) =>{
-  return blogs.reduce(((accumulator,blog)=> accumulator + blog.likes),0)
-}
+const totalLikes = (blogs) => {
+  return blogs.reduce((accumulator, blog) => accumulator + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+  const result = blogs.reduce(
+    (prev, current) => {
+      return current.likes > prev.likes ? current : prev;
+    },
+    { likes: -Infinity }
+  );
+  console.log(result);
+  return { title: result.title, author: result.author, likes: result.likes };
+};
 
 module.exports = {
   dummy,
-  totalLikes
-}
+  totalLikes,
+  favoriteBlog,
+};
