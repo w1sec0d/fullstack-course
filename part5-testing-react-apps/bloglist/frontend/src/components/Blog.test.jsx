8cc771d3d4d28f6d362b3c3f01f090d1bab2af6a
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-09-17 21:42:40 -0500

    Add exercise 5.14: Blog List Tests, step 2

--- part5-testing-react-apps/bloglist/frontend/src/components/Blog.test.jsx
+++ part5-testing-react-apps/bloglist/frontend/src/components/Blog.test.jsx
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react'
-import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 import { expect } from 'vitest'
+
+import Blog from './Blog'
 
 describe('Blog Component',()=>{
   let container
@@ -26,5 +28,15 @@ describe('Blog Component',()=>{
     expect(testDiv).toHaveTextContent('tester')
     expect(testDiv).not.toHaveTextContent('test.com')
     expect(testDiv).not.toHaveTextContent('25')
+  })
+
+  test('Displays url and likes when button is clicked', async()=>{
+    const user = userEvent.setup()
+    const button = screen.getByText("View")
+    await user.click(button)
+
+    const testDiv = container.querySelector(".blog")
+    expect(testDiv).toHaveTextContent('test.com')
+    expect(testDiv).toHaveTextContent('25')
   })
 })
