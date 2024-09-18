a38a4bee2b1daea356d68642a8f7e73b98a8cf98
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-09-17 22:05:36 -0500

    Add exercise 5.15: Blog List Tests, step 3

--- part5-testing-react-apps/bloglist/frontend/src/components/Blog.test.jsx
+++ part5-testing-react-apps/bloglist/frontend/src/components/Blog.test.jsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import { expect } from 'vitest'
+import { expect, test } from 'vitest'
 
 import Blog from './Blog'
 
@@ -38,5 +38,27 @@ describe('Blog Component',()=>{
     const testDiv = container.querySelector(".blog")
     expect(testDiv).toHaveTextContent('test.com')
     expect(testDiv).toHaveTextContent('25')
+  })
+
+  test('When like button is clicked twice, event handler is called twice',async()=>{
+    document.body.innerHTML = '';
+    const likeFunction = vi.fn()
+    const testBlog = {
+      title:"A test blog",
+      author:"tester",
+      url:"test.com",
+      likes:25
+    }
+    container = render(<Blog handleLike={likeFunction} value={testBlog}/>).container
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByText("View")
+    await user.click(viewButton)
+    
+    const likeButton = screen.getByText("Like")
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(likeFunction.mock.calls).toHaveLength(2)
   })
 })
