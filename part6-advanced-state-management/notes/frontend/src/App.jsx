fcb7be91af789e383999e36c2a143f3db6ba5819
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-10-07 22:28:26 -0500

    Create anecdotes redux base

--- part6-advanced-state-management/notes/frontend/src/App.jsx
+++ part6-advanced-state-management/notes/frontend/src/App.jsx
@@ -1,43 +1,12 @@
-import React from 'react'
-import { createNote, toggleImportanceOf } from './reducers/noteReducer'
-import { useSelector, useDispatch } from 'react-redux'
+import React from "react"
+import NewNote from "./components/NewNote"
+import Notes from "./components/Notes"
 
 const App = () => {
-
-  const dispatch = useDispatch() // Sets dispatcher function
-  const notes = useSelector(state => state) // Selects the store to be used
-  
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-
-    dispatch(createNote(content))
-  }
-
-  const toggleImportance = (id) => {
-
-    dispatch(toggleImportanceOf(id))
-  }
-
   return (
     <div>
-      <form onSubmit={addNote}>
-        <input name="note" /> 
-        <button type="submit">add</button>
-      </form>
-      <ul>
-
-        {notes.map(note =>
-          <li
-            key={note.id} 
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-      </ul>
+      <NewNote />
+      <Notes />
     </div>
   )
 }
