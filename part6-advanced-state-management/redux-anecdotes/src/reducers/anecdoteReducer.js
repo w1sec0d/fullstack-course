f435ccea10211a5eeb262f7f9a458f577311e0eb
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-10-08 22:42:41 -0500

    Add exercise 6.3: Anecdotes, step 1

--- part6-advanced-state-management/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6-advanced-state-management/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,22 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch(action.type){
+    case "VOTE":{
+      const id = action.payload.id
+      return state.map((anecdote)=> {
+        if (anecdote.id === id){          
+          return {...anecdote, votes: anecdote.votes + 1}
+        }else{
+          return anecdote
+        }
+      })      
+    }
+    default:{
+      return state
+    }
+  }
+
 }
 
 export default reducer
