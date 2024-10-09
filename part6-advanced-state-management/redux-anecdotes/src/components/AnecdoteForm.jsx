4ec286395824b0d07d5076ad4737df2fce5e17c0
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-10-08 23:40:59 -0500

    Add exercise 6.7: Anecdotes, step 5

---
+++ part6-advanced-state-management/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,22 @@
+import { useDispatch } from "react-redux"
+import { createNote } from "../reducers/anecdoteReducer"
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+
+  const addNote = (event) => {
+    event.preventDefault()
+    const note = event.target.note.value
+    event.target.note.value = ""
+    dispatch(createNote(note))
+  }
+
+  return (
+    <form onSubmit={addNote}>
+        <div><input name="note"/></div>
+        <button>create</button>
+    </form>
+  )
+}
+
+export default AnecdoteForm
