9cab9485d5c749538bcf7ef96cd20510edd7e049
Carlos Ramirez <cramirezmun@unal.edu.co>
2024-10-09 20:56:59 -0500

    Add createSlice usage

--- part6-advanced-state-management/redux-anecdotes/src/main.jsx
+++ part6-advanced-state-management/redux-anecdotes/src/main.jsx
@@ -1,11 +1,14 @@
 import App from './App'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const store = configureStore({reducer : combineReducers({anecdotes: anecdoteReducer, filter: filterReducer})})
+const store = configureStore({
+  anecdotes: anecdoteReducer, 
+  filter: filterReducer
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
