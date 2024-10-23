d37ae2da9104d994a590af711c195ed0b46f1019
Carlos Ramírez <cramirezmun@unal.edu.co>
2024-10-23 16:48:46 -0500

    Add exercise 7.2: Routed Anecdotes, step 2

--- part7-react-router-and-more/routed-anecdotes/src/App.jsx
+++ part7-react-router-and-more/routed-anecdotes/src/App.jsx
@@ -18,7 +18,7 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <Link key={anecdote.id} to={"/anecdotes/" + anecdote.id}><li>{anecdote.content}</li></Link>)}
+      {anecdotes.map(anecdote => <Link key={anecdote.id} to={'/anecdotes/' + anecdote.id}><li>{anecdote.content}</li></Link>)}
     </ul>
   </div>
 )
